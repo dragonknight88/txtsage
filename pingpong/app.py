5217a5c7c2cdfbc4d99417c72d07da229801aa32
dragonknight88 <pratikpawar88@gmail.com>
2016-11-24 00:08:06 -0800

    Update app.py

--- pingpong/app.py
+++ pingpong/app.py
@@ -3,7 +3,19 @@ from rapidsms.apps.base import AppBase
 class PingPong(AppBase):
 
     def handle(self, msg):
-        if msg.text == 'ping':
-            msg.respond('pong')
+        
+        
+        if msg.text == "book":
+            msg.respond('Hello user! May I know your zipcode for the booking?')
             return True
+        elif msg.text == "lawrence":
+            msg.respond('Hey Lawrence How are you doing?')
+            return True
+        elif  msg.text == "pratik":
+            msg.respond('Hey Pratik How are you doing?')
+            return True
+        elif  msg.text == "shankar":
+            msg.respond('Hey Shankar How are you doing?')
+            return True
+                
         return False
